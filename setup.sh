6433566f851302b578622b9cc22e39f5d9a358e5
Fabian Foerg <ffoerg@gdssecurity.com>
2015-07-30 19:14:24 -0400

    Made checksum check more portable

--- setup.sh
+++ setup.sh
@@ -7,13 +7,7 @@ wget http://mirrors.nycbug.org/pub/OpenBSD/OpenSSH/portable/openssh-6.9p1.tar.gz
 #git clone -b V_6_9_P1 https://anongit.mindrot.org/openssh.git
 tar xzf openssh-6.?p?.tar.gz
 
-CHECKSUM=""
-
-if [ `uname -s` == "Darwin" ]; then
-    CHECKSUM=$(shasum -a 256 openssh-6.?p?.tar.gz)
-else
-    CHECKSUM=$(sha256sum openssh-6.?p?.tar.gz)
-fi
+CHECKSUM=$(shasum -a 256 openssh-6.?p?.tar.gz)
 
 if [ "$CHECKSUM" != "6e074df538f357d440be6cf93dc581a21f22d39e236f217fcd8eacbb6c896cfe  openssh-6.9p1.tar.gz" ]; then
   echo "Error: Checksum check failed!"
