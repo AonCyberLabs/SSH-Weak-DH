03bc2f1ff33111ed1ead4787c941ffc0ca95ed64
Ron Gutierrez <rgutierrez@gdssecurity.com>
2015-07-30 18:52:21 -0400

    updated setup.sh to support mac

--- setup.sh
+++ setup.sh
@@ -6,11 +6,21 @@ patch -p1 < logjam-configs.patch
 wget http://mirrors.nycbug.org/pub/OpenBSD/OpenSSH/portable/openssh-6.9p1.tar.gz
 #git clone -b V_6_9_P1 https://anongit.mindrot.org/openssh.git
 tar xzf openssh-6.?p?.tar.gz
-CHECKSUM=$(sha256sum openssh-6.?p?.tar.gz)
+
+CHECKSUM=""
+
+if [ `uname -s` == "Darwin" ]; then
+    CHECKSUM=$(shasum -a 256 openssh-6.?p?.tar.gz)
+else
+    CHECKSUM=$(sha256sum openssh-6.?p?.tar.gz)
+fi
+
 if [ "$CHECKSUM" != "6e074df538f357d440be6cf93dc581a21f22d39e236f217fcd8eacbb6c896cfe  openssh-6.9p1.tar.gz" ]; then
   echo "Error: Checksum check failed!"
   exit 1
 fi
+
+
 cd openssh-6.?p?
 patch -p1 < ../openssh.patch
 ./configure
