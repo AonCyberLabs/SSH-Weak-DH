cd0dbed19f802dd448544b5b0e5935eb38685682
Fabian Foerg <ffoerg@gdssecurity.com>
2015-07-31 10:49:38 -0400

    Improved output of analysis script.

--- logjam-analyze.py
+++ logjam-analyze.py
@@ -11,43 +11,68 @@ from __future__ import print_function
 import sys
 from os import listdir
 from os.path import isfile, isdir, join
+import re
 
 DH_BITS_WEAK = 768
 DH_BITS_ACADEMIC = 1024
 DH_BITS_NATION = 1536
-DH_GROUP_BIT_LOG_STRING = "KEX server-chosen group size in bits: "
+KEX_ALGO = "KEX algorithm chosen: "
+DH_GROUP_BIT_CLIENT = "KEX client group sizes: "
+DH_GROUP_BIT_SERVER = "KEX server-chosen group size in bits: "
 
 """
 prints a security ranking for the given Diffie-Hellman group size
 in bits
 """
-def dh_sec_level(dh_bits):
-    print("The Diffie-Hellman group exchange negotiated a group size of",
-            dh_bits, "bits.")
-    print("The security is ", end="")
-    if dh_bits < DH_BITS_WEAK:
+def dh_sec_level(dh_algo, dh_bits_client, dh_bits_server):
+    print("The client proposed the following group size parameters (in bits): min=", dh_bits_client[0], ", nbits=", dh_bits_client[1], ", max=", dh_bits_client[2], ".", sep='')
+    print("The client and server negotiated a group size of ",
+            dh_bits_server, " using ", dh_algo, ".", sep='')
+    print("The security level is ", end="")
+    if dh_bits_server < DH_BITS_WEAK:
         print("WEAK.")
-    elif dh_bits < DH_BITS_ACADEMIC:
-        print("WEAK-INTERMEDIATE (an academic team might be able to break the security).")
-    elif dh_bits < DH_BITS_NATION:
-        print("INTERMEDIATE (a nation-state might be able to break the security).")
+    elif dh_bits_server < DH_BITS_ACADEMIC:
+        print("WEAK-INTERMEDIATE (might be feasible to break for academic teams).")
+    elif dh_bits_server < DH_BITS_NATION:
+        print("INTERMEDIATE (might be feasible to break for nation-states).")
     else:
         print("STRONG.")
+    print()
 
 """
-analyze the given file, looking for the chosen Diffie-Hellman group size
+analyze the given file, looking for Diffie-Hellman group sizes and
+algorithm
 """
 def analyze(f):
     file = open(f, "r")
+    lines = file.readlines()
+    lineno = 0
+    dh_algo = ""
+    dh_bits_client = (0, 0, 0)
+    dh_bits_server = 0
 
-    for line in file.readlines():
-        if line.startswith(DH_GROUP_BIT_LOG_STRING):
+    while (lineno + 2) < len(lines):
+        line = lines[lineno]
+        if line.startswith(KEX_ALGO):
+            dh_algo = line[len(KEX_ALGO):].strip()
+        line = lines[lineno + 1]
+        if line.startswith(DH_GROUP_BIT_CLIENT):
+            ints = [int(s) for s in re.split('\s+|\s*,\s*', line) if s.isdigit()]
+            if len(ints) == 3:
+                dh_bits_client = ints
+            else:
+                print("Error: Cannot parse client parameters!")
+                break
+        line = lines[lineno + 2]
+        if line.startswith(DH_GROUP_BIT_SERVER):
             ints = [int(s) for s in line.split() if s.isdigit()]
             if len(ints) == 1:
-                dh_bits = ints[0]
-                dh_sec_level(dh_bits)
+                dh_bits_server = ints[0]
+                dh_sec_level(dh_algo, dh_bits_client, dh_bits_server)
             else:
-                print("Error: Cannot parse Diffie-Hellman group size!")
+                print("Error: Cannot parse server group size!")
+                break
+        lineno += 1
 
 """
 analyze all files in the given directory
@@ -66,7 +91,7 @@ def main():
     args = sys.argv
 
     if len(args) != 2:
-        print("Syntax: python -u ", args[0], " directory")
+        print("Syntax: python -u", args[0], "directory")
         exit(1)
     else:
        directory = args[1]
