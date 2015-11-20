847cf397070d512a07ea2c40b49aa7b5a4d0ddd5
Fabian Foerg <me@ffoerg.de>
2015-11-20 18:04:05 -0500

    Do not download OpenSSH client archive again, if it is already there.

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # sudo apt-get install build-essential automake libssl-dev
-wget http://mirrors.nycbug.org/pub/OpenBSD/OpenSSH/portable/openssh-6.9p1.tar.gz
+wget -nc http://mirrors.nycbug.org/pub/OpenBSD/OpenSSH/portable/openssh-6.9p1.tar.gz
 #git clone -b V_6_9_P1 https://anongit.mindrot.org/openssh.git
 tar xzf openssh-6.?p?.tar.gz
 
@@ -11,7 +11,6 @@ if [ "$CHECKSUM" != "6e074df538f357d440be6cf93dc581a21f22d39e236f217fcd8eacbb6c8
   echo "Error: Checksum check failed!"
   exit 1
 fi
-
 
 cd openssh-6.?p?
 patch -p1 < ../openssh.patch
