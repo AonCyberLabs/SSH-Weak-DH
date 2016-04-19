da7a30832a473640b4e96e6aea735b6ed063625a
Anders Kaseorg <andersk@mit.edu>
2016-04-19 18:31:41 -0400

    setup.sh: Verify openssh tarball checksum before extracting it

--- setup.sh
+++ setup.sh
@@ -10,7 +10,6 @@ command -v make >/dev/null 2>&1 || { echo >&2 "Please install make"; exit 1; }
 # Installation
 wget -nc http://mirrors.nycbug.org/pub/OpenBSD/OpenSSH/portable/openssh-6.9p1.tar.gz
 #git clone -b V_6_9_P1 https://anongit.mindrot.org/openssh.git
-tar xzf openssh-6.?p?.tar.gz
 
 CHECKSUM=$(shasum -a 256 openssh-6.?p?.tar.gz)
 if [ "$CHECKSUM" != "6e074df538f357d440be6cf93dc581a21f22d39e236f217fcd8eacbb6c896cfe  openssh-6.9p1.tar.gz" ]; then
@@ -18,6 +17,7 @@ if [ "$CHECKSUM" != "6e074df538f357d440be6cf93dc581a21f22d39e236f217fcd8eacbb6c8
   exit 1
 fi
 
+tar xzf openssh-6.?p?.tar.gz
 cd openssh-6.?p?
 patch -p1 < ../openssh.patch
 ./configure
