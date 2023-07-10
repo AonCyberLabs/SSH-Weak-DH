ef67e24726690d4e00816d28081657c6965b8944
Fabian Foerg <3429782+faf0@users.noreply.github.com>
2023-07-09 21:23:26 -0400

    Clean Up Test Shell Script

--- resources/ssh-weak-dh-test.sh
+++ resources/ssh-weak-dh-test.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 
 readonly SSH_PATCHED='./ssh'
-readonly SSH_OPTS='-2 -T -o LogLevel=error -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -o NoHostAuthenticationForLocalhost=yes'
-readonly CONFIGS='configs/config-default configs/config-dh_gex_sha1 configs/config-dh_gex_sha256'
-readonly BIT_LENGTHS='512 768 1024 1280 1536 2048'
+readonly SSH_OPTS=(-2 -T -o LogLevel=error -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -o NoHostAuthenticationForLocalhost=yes)
+readonly CONFIGS_GROUP_EXCHANGE=(configs/config-dh_gex_sha1 configs/config-dh_gex_sha256)
+readonly CONFIGS_GROUP_FIXED=(configs/config-group1 configs/config-group14-sha1 configs/config-group14-sha256 configs/config-group16 configs/config-group18)
+readonly BIT_LENGTHS=(512 768 1024 1280 1536 2048)
 readonly OUT_DIR='/logs'
 readonly SSH_WEAK_DH_ANALYZE='./ssh-weak-dh-analyze.py'
 
@@ -25,14 +26,16 @@ run() {
   local target=$2
   local port=$3
   local out_prefix=$4
-  local out_file_name=$(basename $config)
+  local out_file_name
+
+  out_file_name=$(basename "$config")
 
   # redirect stderr containing key exchange information to file
   if [[ "$#" -ne 5 ]]; then
-    $SSH_PATCHED $SSH_OPTS -F $config -p $port $target 2>&1 | tee "${out_prefix}/${out_file_name}"
+    "$SSH_PATCHED" "${SSH_OPTS[@]}" -F "$config" -p "$port" "$target" 2>&1 | tee "${out_prefix}/${out_file_name}"
   else
     local bit_length=$5
-    $SSH_PATCHED $SSH_OPTS -d minbits=${bit_length},nbits=${bit_length},maxbits=${bit_length} -F $config -p $port $target 2>&1 | tee "${out_prefix}/${out_file_name}_${bit_length}"
+    "$SSH_PATCHED" "${SSH_OPTS[@]}" -d "minbits=${bit_length},nbits=${bit_length},maxbits=${bit_length}" -F "$config" -p "$port" "$target" 2>&1 | tee "${out_prefix}/${out_file_name}_${bit_length}"
   fi
 }
 
@@ -41,7 +44,7 @@ main() {
   local port=22
 
   if [[ "$#" -ne 1 ]] && [[ "$#" -ne 2 ]]; then
-    usage $0
+    usage "$0"
     exit 1
   fi
   # port specified?
@@ -54,22 +57,25 @@ main() {
   local port_clean="${port%%[^0-9]*}"
   local out_prefix="${OUT_DIR}/${target_clean}-${port_clean}"
 
-  mkdir -p "${out_prefix}"
+  mkdir -p "$out_prefix"
 
-  echo "Saving output files in ${out_prefix}"
+  echo "Saving output files in $out_prefix"
   echo ""
 
-  # Check for group1 manually
-  $SSH_PATCHED $SSH_OPTS -F configs/config-group1 -p $port $target 2>&1 | tee "${out_prefix}/config-group1"
-
-  for config in $CONFIGS
+  for config in "${CONFIGS_GROUP_FIXED[@]}"
   do
-    # Tests with default OpenSSH client parameters
-    run $config $target $port $out_prefix
-    # Test some specific parameters
-    for bit_length in ${BIT_LENGTHS}
+    run "$config" "$target" "$port" "$out_prefix"
+  done
+
+  for config in "${CONFIGS_GROUP_EXCHANGE[@]}"
+  do
+    # Tests with default OpenSSH client parameters:
+    run "$config" "$target" "$port" "$out_prefix"
+
+    # Test some specific parameters:
+    for bit_length in "${BIT_LENGTHS[@]}"
     do
-      run $config $target $port $out_prefix $bit_length
+      run "$config" "$target" "$port" "$out_prefix" "$bit_length"
     done
   done
 
@@ -78,7 +84,7 @@ main() {
   echo "Analysis of results:"
   echo ""
 
-  pipenv run --quiet python -u $SSH_WEAK_DH_ANALYZE $out_prefix
+  pipenv run --quiet python -u "$SSH_WEAK_DH_ANALYZE" "$out_prefix"
 }
 
 main "$@"
